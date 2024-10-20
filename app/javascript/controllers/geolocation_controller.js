6f63e215d4adceff17ce261303c1102a11ed67fb
heidless <lockhart.r@gmail.com>
2024-10-20 18:44:01 +0100

    distance based on user geo

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,17 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
+import { _isEmpty } from 'lodash-es'
 
 export default class extends Controller {
 
   connect() {
-    if (this.element.dataset.latitude && this.element.dataset.longitude) {
-
-
-    } else {}
-    window.navigator.geolocation.getCurrentPosition((position) => {
-      console.log('this.element: ', this.element); 
-      this.element.dataset.latitude = position.coords.latitude;
-      this.element.dataset.longitude = position.coords.longitude; 
-    })
+    if (isEmpty(his.element.dataset.latitude) && isEmpty(this.element.dataset.longitude)) {
+      window.navigator.geolocation.getCurrentPosition((position) => {
+        console.log('this.element: ', this.element); 
+        this.element.dataset.latitude = position.coords.latitude;
+        this.element.dataset.longitude = position.coords.longitude; 
+      })
+    } else {
+    }
   }
 }
 
