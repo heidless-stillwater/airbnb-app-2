a5be49e99a32297005778993b9028d2eb2cee0c6
heidless <lockhart.r@gmail.com>
2024-10-21 11:01:54 +0100

    functional base

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,17 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
-import { _isEmpty } from 'lodash-es'
 
 export default class extends Controller {
 
   connect() {
-    if (isEmpty(his.element.dataset.latitude) && isEmpty(this.element.dataset.longitude)) {
+    console.log('geolocation: ')
       window.navigator.geolocation.getCurrentPosition((position) => {
         console.log('this.element: ', this.element); 
         this.element.dataset.latitude = position.coords.latitude;
         this.element.dataset.longitude = position.coords.longitude; 
       })
-    } else {
-    }
   }
 }
 
