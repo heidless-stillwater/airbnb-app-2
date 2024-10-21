53e0d99b46b6b88de5ccba8ff859536262639dc2
heidless <lockhart.r@gmail.com>
2024-10-21 11:46:56 +0100

    functional

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -3,12 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   connect() {
-    console.log('geolocation: ')
+    if (this.element.dataset.latitude && this.element.dataset.latitude) {
+
+    } else {
       window.navigator.geolocation.getCurrentPosition((position) => {
         console.log('this.element: ', this.element); 
         this.element.dataset.latitude = position.coords.latitude;
         this.element.dataset.longitude = position.coords.longitude; 
       })
+    }
   }
 }
 
